87d60afadea4963a03fe25c4da35a45dbbc14f28
Nako <Nanaka.kumiho@gmail.com>
2023-05-02 12:12:45 +0200

    clean up base project

--- Colors/color.go
+++ Colors/color.go
@@ -1,44 +1,44 @@
 package Colors
 
-import "github.com/gookit/color"
+// import "github.com/gookit/color"
 
-func GetColor(myColor string) func(string) {
-	var ColorFunc func(string)
+// func GetColor(myColor string) func(string) {
+// 	var ColorFunc func(string)
 
-	switch myColor {
-	case "red":
-		ColorFunc = func(val string) {
-			color.Red.Print(val)
-		}
-	case "green":
-		ColorFunc = func(val string) {
-			color.Green.Print(val)
-		}
-	case "yellow":
-		ColorFunc = func(val string) {
-			color.Yellow.Print(val)
-		}
-	case "blue":
-		ColorFunc = func(val string) {
-			color.Blue.Print(val)
-		}
-	case "purple":
-		ColorFunc = func(val string) {
-			color.FgMagenta.Print(val)
-		}
-	case "cyan":
-		ColorFunc = func(val string) {
-			color.Cyan.Print(val)
-		}
-	case "white":
-		ColorFunc = func(val string) {
-			color.White.Print(val)
-		}
-	default:
-		ColorFunc = func(val string) {
-			print(val)
-		}
-	}
+// 	switch myColor {
+// 	case "red":
+// 		ColorFunc = func(val string) {
+// 			color.Red.Print(val)
+// 		}
+// 	case "green":
+// 		ColorFunc = func(val string) {
+// 			color.Green.Print(val)
+// 		}
+// 	case "yellow":
+// 		ColorFunc = func(val string) {
+// 			color.Yellow.Print(val)
+// 		}
+// 	case "blue":
+// 		ColorFunc = func(val string) {
+// 			color.Blue.Print(val)
+// 		}
+// 	case "purple":
+// 		ColorFunc = func(val string) {
+// 			color.FgMagenta.Print(val)
+// 		}
+// 	case "cyan":
+// 		ColorFunc = func(val string) {
+// 			color.Cyan.Print(val)
+// 		}
+// 	case "white":
+// 		ColorFunc = func(val string) {
+// 			color.White.Print(val)
+// 		}
+// 	default:
+// 		ColorFunc = func(val string) {
+// 			print(val)
+// 		}
+// 	}
 
-	return ColorFunc
-}
+// 	return ColorFunc
+// }
--- Typo/UseTxt.go
+++ Typo/UseTxt.go
@@ -1,79 +1,79 @@
 package Typo
 
-import (
-	"bufio"
-	"io/ioutil"
-	"os"
-)
+// import (
+// 	"bufio"
+// 	"io/ioutil"
+// 	"os"
+// )
 
-func AskChar(typo string, char rune) []string {
-	var toReturn []string
-	char -= 32
+// func AskChar(typo string, char rune) []string {
+// 	var toReturn []string
+// 	char -= 32
 
-	files := ListFile()
-	var lines []string
+// 	files := ListFile()
+// 	var lines []string
 
-	for _, val := range files {
-		if val == typo {
-			val = "./Typo/" + val + ".txt"
-			if !ReadLines(val, &lines) {
-				return toReturn
-			}
-		}
-	}
-	if lines == nil {
-		print("aucun fichier correspondant à la typo demandée")
-	}
+// 	for _, val := range files {
+// 		if val == typo {
+// 			val = "./Typo/" + val + ".txt"
+// 			if !ReadLines(val, &lines) {
+// 				return toReturn
+// 			}
+// 		}
+// 	}
+// 	if lines == nil {
+// 		print("aucun fichier correspondant à la typo demandée")
+// 	}
 
-	toReturn = GetTxtChar(int(char), lines)
+// 	toReturn = GetTxtChar(int(char), lines)
 
-	return toReturn
-}
+// 	return toReturn
+// }
 
-func ListFile() []string {
-	files := []string{""}
+// func ListFile() []string {
+// 	files := []string{""}
 
-	fileList, _ := ioutil.ReadDir("./Typo")
+// 	fileList, _ := ioutil.ReadDir("./Typo")
 
-	for i := range fileList {
-		val := fileList[i].Name()
-		if len(val) > 4 {
-			if val[len(val)-4:] == ".txt" {
-				files = append(files, val[:len(val)-4])
-			}
-		}
-	}
-	return files
-}
+// 	for i := range fileList {
+// 		val := fileList[i].Name()
+// 		if len(val) > 4 {
+// 			if val[len(val)-4:] == ".txt" {
+// 				files = append(files, val[:len(val)-4])
+// 			}
+// 		}
+// 	}
+// 	return files
+// }
 
-func ReadLines(fileName string, lines *[]string) bool {
-	file, hasError := os.Open(fileName)
-	if hasError != nil {
-		print("Erreur de lecture de fichier")
-		return false
-	}
-	var content []string
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
+// func ReadLines(fileName string, lines *[]string) bool {
+// 	file, hasError := os.Open(fileName)
+// 	if hasError != nil {
+// 		print("Erreur de lecture de fichier")
+// 		return false
+// 	}
+// 	var content []string
+// 	defer file.Close()
+// 	scanner := bufio.NewScanner(file)
 
-	for scanner.Scan() {
-		content = append(content, scanner.Text())
-	}
-	*lines = content
-	return true
-}
+// 	for scanner.Scan() {
+// 		content = append(content, scanner.Text())
+// 	}
+// 	*lines = content
+// 	return true
+// }
 
-func GetTxtChar(char int, lines []string) []string {
-	var toReturn []string
-	incremente := 0
-	for i := 1; i < len(lines); i++ {
-		if lines[i] == "" {
-			incremente++
-		}
-		if incremente == char {
-			toReturn = lines[i+1 : i+10]
-			break
-		}
-	}
-	return toReturn
-}
+// func GetTxtChar(char int, lines []string) []string {
+// 	var toReturn []string
+// 	incremente := 0
+// 	for i := 1; i < len(lines); i++ {
+// 		if lines[i] == "" {
+// 			incremente++
+// 		}
+// 		if incremente == char {
+// 			toReturn = lines[i+1 : i+10]
+// 			break
+// 		}
+// 	}
+// 	return toReturn
+// }
